f77881debf77d58eef84bfd987df1284a909e158
Sergey Bibikov <sergey.bibikov.92@mail.ru>
2022-04-10 22:41:55 +0300

    tokens

--- main.go
+++ main.go
@@ -9,11 +9,40 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/ready", readinessCheck)
+	router.POST("/token/get", getToken)
+	router.POST("/token/check", checkToken)
 	router.Run()
 }
 
 func readinessCheck(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"status": "ready",
+	})
+}
+
+func getToken(c *gin.Context) {
+	var body map[string]string
+	c.BindJSON(&body)
+	if body["username"] == "valid_user" && body["password"] == "valid_password" {
+		c.JSON(http.StatusCreated, gin.H{
+			"success":      true,
+			"refreshToken": "ref_resh",
+			"accessToken":  "access_t",
+		})
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"success":      false,
+			"refreshToken": "",
+			"accessToken":  "",
+		})
+	}
+}
+
+func checkToken(c *gin.Context) {
+	var body map[string]string
+	c.BindJSON(&body)
+	c.JSON(http.StatusOK, gin.H{
+		"refreshToken": "ref_resh",
+		"accessToken":  "access_t",
 	})
 }
