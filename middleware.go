6a5dd4342a2f8949d13c6ceb1d7431d07b70e3f9
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-09-21 18:44:18 +0300

    redundant middleware deleted

--- middleware.go
+++ middleware.go
@@ -2,20 +2,6 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func checkTokenIsPresent(c *gin.Context) {
-	tok := c.GetHeader("Authrization")
-	if tok == "" {
-		c.JSON(401, gin.H{"error": "no token provided"})
-		return
-	}
-	_, err := parseToken(tok)
-	if err != nil {
-		c.JSON(401, gin.H{"error": err.Error})
-		return
-	}
-	c.Next()
-}
-
 func checkIsAdmin(c *gin.Context) {
 	t := c.GetHeader("Authrization")
 	tok, err := parseToken(t)
