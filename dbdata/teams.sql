3e10c0083788d41891038f59c315f1f2e896004b
SergeyBibikov <sergey.bibikov.92@mail.ru>
2023-01-13 22:31:22 +0400

    teams checks

--- dbdata/teams.sql
+++ dbdata/teams.sql
@@ -1,9 +1,9 @@
 CREATE TABLE TEAMS(
     ID serial primary key,
-    NAME VARCHAR(100) NOT NULL,
-    CONFERENCE VARCHAR(100) NOT NULL,
-    DIVISION VARCHAR(100) NOT NULL,
-    EST_YEAR INT NOT NULL);
+    NAME VARCHAR(100) NOT NULL CHECK(length(name)>3),
+    CONFERENCE VARCHAR(100) NOT NULL CHECK(conference in ('East', 'West')),
+    DIVISION VARCHAR(100) NOT NULL CHECK(division in ('Atlantic', 'Pacific', 'Southeast', 'Central', 'Northwest', 'Southwest')),
+    EST_YEAR INT NOT NULL CHECK(est_year > 1940));
 
 
 COPY teams(NAME, CONFERENCE, DIVISION, EST_YEAR) FROM '/docker-entrypoint-initdb.d/teams.csv' CSV HEADER;
